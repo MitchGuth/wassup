a3fe28f16d10ed0c69bd85478d476cfcb833584c
Mitch Guth <mdguth@gmail.com>
2018-09-18 11:45:55 -0400

    updated Link

--- src/components/IndividualChurp.js
+++ src/components/IndividualChurp.js
@@ -2,5 +2,6 @@ import React, { Component } from 'react';
 let IndividualChurp = (props) => 
     <li>
         <p className='individual-churp'>Churp: {props.churp.content}</p>
+        {/* <Link to="`/feed/${props.churp.id}`">Show More</Link> */}
     </li>
 export default IndividualChurp;
